d2058b11fc140fc2bc90690cdd1c249cc03dafed
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-06-05 12:56:52 -0700

    call childrens acc class

--- children_account.py
+++ children_account.py
@@ -5,7 +5,7 @@ class Children_Account(Account):
         super().__init__(account_number, account_holder, balance)
         self.__interest_rate = 0.7 / 100
     def get_interest_rate(self):
-        return self.__interest_rate
+        return self.__interest_rate * 100
     def set_interest_rate(self, interest_rate):
         self.__interest_rate = interest_rate / 100
         return self.__interest_rate * 100
@@ -14,6 +14,12 @@ class Children_Account(Account):
     def deposit(self, amount):
         return super().deposit(amount)
     def add_interest(self):
-        interest = self.balance * self.__interest_rate
+        interest_rate = self.get_interest_rate()
+        interest = self.balance * interest_rate
         self.deposit(interest)
         return interest
+    
+my_childs_account = Children_Account(9124587748, "Bemdoo Maor")
+my_childs_account.withdraw(3000)
+my_childs_account.deposit(5000)
+my_childs_account.add_interest()
